3556cb02cd2e62a78fc2bd9de36b049fca6981f6
zakaria-akash <zakaria.93@yahoo.com>
2022-12-01 14:25:32 +0600

    adding main-header component and its styling module; render main-header component for every pages through -app.js

---
+++ components/layout/main-header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Link from "next/link";
+
+import classes from "./main-header.module.css";
+
+const MainHeader = (props) => {
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>
+        <Link href="/">Next Event</Link>
+      </div>
+      <nav className={classes.navigation}>
+        <ul>
+          <li>
+            <Link href="/events">Browse All Events</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainHeader;
