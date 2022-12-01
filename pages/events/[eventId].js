988623f10b2cbfb6c61f9d27fe175daae7d6e682
zakaria-akash <zakaria.93@yahoo.com>
2022-12-01 11:47:01 +0600

    adding some regular react components such as event-content, event-logistics, event-summary and logistics-item and their correspondins css module; using these components in the dynamic eventId page to render the details information for any specific event

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { useRouter } from "next/router";
+
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
 
 const EventDetailPage = () => {
-  return (
-    <div>
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return (
       <center>
-        <h1>Event Detail Page</h1>
+        <h1>No event found!</h1>
       </center>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <Fragment>
+        <EventSummary title={event.title} />
+        <EventLogistics
+          date={event.date}
+          address={event.location}
+          image={event.image}
+          imageAlt={event.title}
+        />
+        <EventContent>
+          <p>{event.description}</p>
+        </EventContent>
+      </Fragment>
+    );
+  }
 };
 
 export default EventDetailPage;
