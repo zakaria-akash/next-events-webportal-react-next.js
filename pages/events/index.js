79aeab77bec238f754dd191fde37f6c632ddca60
zakaria-akash <zakaria.93@yahoo.com>
2022-12-02 00:36:36 +0600

    adding a search field in the AllEvents page to search for an event by year and month

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { Fragment } from "react";
+import EventList from "../../components/events/event-list";
+
+import { getAllEvents } from "../../dummy-data";
+import EventsSearch from "./events-search";
 
 const AllEventsPage = () => {
+  const events = getAllEvents();
   return (
-    <div>
-      <center>
-        <h1>All Events Page</h1>
-      </center>
-    </div>
+    <Fragment>
+      <EventsSearch />
+      <EventList items={events} />
+    </Fragment>
   );
 };
 
