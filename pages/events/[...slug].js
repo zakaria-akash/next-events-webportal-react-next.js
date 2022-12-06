e76fe9167c7932d3a150a01c0fe8f1f33fd3c92f
zakaria-akash <zakaria.93@yahoo.com>
2022-12-06 20:59:46 +0600

    replacing serverside rendering with client side data fetching approach with the help of useEffect and SWR hooks

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,64 +1,45 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import useSWR from "swr";
 
-import { getFilteredEvents } from "../../backend-helpers/api-utils";
 import EventList from "../../components/events/event-list";
 import ResultsTitle from "../../components/events/results-title";
 import Button from "../../components/ui/button";
 import ErrorAlert from "../../components/ui/error-alert";
 
-const FilteredEventsPage = (props) => {
+const FilteredEventsPage = () => {
+  const [loadedEvents, setLoadedEvents] = useState();
   const router = useRouter();
 
-  // const filteredData = router.query.slug;
+  const filteredData = router.query.slug;
 
-  // if (!filteredData) {
-  //   return <h3 className="center">Loading...</h3>;
-  // }
-
-  // const filteredYear = filteredData[0];
-  // const filteredMonth = filteredData[1];
-
-  // const numYear = +filteredYear;
-  // const numMonth = +filteredMonth;
-
-  if (props.hasError) {
-    return (
-      <ErrorAlert>
-        <div className="center">
-          <h4>Invalid URL to show any specific event!!!</h4>
-          <Button link="/events">Show All Events</Button>
-        </div>
-      </ErrorAlert>
-    );
-  }
-
-  const filteredEvents = props.events;
-
-  if (!filteredEvents || filteredEvents.length === 0) {
-    return (
-      <ErrorAlert>
-        <div className="center">
-          <h4>No event found for the chosen date</h4>
-          <Button link="/events">Show All Events</Button>
-        </div>
-      </ErrorAlert>
-    );
-  }
-
-  const date = new Date(props.date.year, props.date.month - 1);
-  return (
-    <Fragment>
-      <ResultsTitle date={date} />
-      <EventList items={filteredEvents} />
-    </Fragment>
+  const { data, error } = useSWR(
+    "https://next-events-portal-default-rtdb.firebaseio.com/events.json",
+    (url) => fetch(url).then((res) => res.json()) //fetcher function for SWR hook
   );
-};
+  if (error) {
+    return <h3 className="center">Failed to load...</h3>;
+  }
+  useEffect(() => {
+    if (data) {
+      const events = [];
+      console.log("Data is available");
 
-export const getServerSideProps = async (context) => {
-  const { params } = context;
+      for (const key in data) {
+        events.push({
+          id: key,
+          ...data[key],
+        });
+      }
 
-  const filteredData = params.slug;
+      setLoadedEvents(events);
+    }
+  }, [data]);
+
+  if (!loadedEvents) {
+    return <h3 className="center">Loading...</h3>;
+  }
+
   const filteredYear = filteredData[0];
   const filteredMonth = filteredData[1];
 
@@ -71,33 +52,46 @@ export const getServerSideProps = async (context) => {
     numYear > 2030 ||
     numYear < 2021 ||
     numMonth > 12 ||
-    numMonth < 1
+    numMonth < 1 ||
+    error
   ) {
-    return {
-      props: {
-        hasError: true,
-      },
-      // notFound: true,
-      // redirect: {
-      //   destination: "/error",
-      // },
-    };
+    return (
+      <ErrorAlert>
+        <div className="center">
+          <h4>Invalid URL to show any specific event!!!</h4>
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </ErrorAlert>
+    );
   }
 
-  const filteredEvents = await getFilteredEvents({
-    year: numYear,
-    month: numMonth,
+  const filteredEvents = loadedEvents.filter((event) => {
+    const eventDate = new Date(event.date);
+    return (
+      eventDate.getFullYear() === numYear &&
+      eventDate.getMonth() === numMonth - 1
+    );
   });
 
-  return {
-    props: {
-      events: filteredEvents,
-      date: {
-        year: numYear,
-        month: numMonth,
-      },
-    },
-  };
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return (
+      <ErrorAlert>
+        <div className="center">
+          <h4>No event found for the chosen date</h4>
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </ErrorAlert>
+    );
+  }
+
+  const date = new Date(numYear, numMonth - 1);
+  return (
+    <Fragment>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
+    </Fragment>
+  );
 };
+
 
 export default FilteredEventsPage;
