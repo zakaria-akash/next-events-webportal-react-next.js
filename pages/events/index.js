74ca6df759f8fd844c9ca782894b0b765828c889
zakaria-akash <zakaria.93@yahoo.com>
2022-12-06 10:42:13 +0600

    render all events in the All Events Page through static generation

--- pages/events/index.js
+++ pages/events/index.js
@@ -3,11 +3,11 @@ import { useRouter } from "next/router";
 
 import EventList from "../../components/events/event-list";
 import EventsSearch from "./events-search";
-import { getAllEvents } from "../../dummy-data";
+import { getAllEvents } from "../../backend-helpers/api-utils";
 
-const AllEventsPage = () => {
+const AllEventsPage = (props) => {
   const router = useRouter();
-  const events = getAllEvents();
+  const { events } = props;
 
   const findEventsHandler = (year, month) => {
     const fullPath = `/events/${year}/${month}`;
@@ -20,6 +20,17 @@ const AllEventsPage = () => {
       <EventList items={events} />
     </Fragment>
   );
+};
+
+export const getStaticProps = async () => {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events: events,
+    },
+    revalidate: 1800,
+  };
 };
 
 export default AllEventsPage;
