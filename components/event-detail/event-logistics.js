0dccc351f4ede083234261f494c6832f737b824f
zakaria-akash <zakaria.93@yahoo.com>
2022-12-07 12:32:43 +0600

    reimport Image tag from next/dist/client/image

--- components/event-detail/event-logistics.js
+++ components/event-detail/event-logistics.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Image from "next/image";
+import Image from "next/dist/client/image";
 
 import AddressIcon from "../icons/address-icon";
 import DateIcon from "../icons/date-icon";
--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Image from "next/image";
+import Image from "next/dist/client/image";
 
 import classes from "./event-item.module.css";
 import Button from "../ui/button";
